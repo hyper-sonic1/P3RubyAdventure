10c6b6b42a812cff458bc4fa98d30e979d976643
hyper-sonic1 <737509@student.dusd.net>
2023-11-15 11:40:54 -0800

    hi my gus but you can call me sus
    vote this guy

--- P3RubysAdventureChase/Assets/scripts/RubyController.cs
+++ P3RubysAdventureChase/Assets/scripts/RubyController.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
-{
+{ 
+    public int maxHealth = 5;
+    int currentHealth;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+    }
+    void FixedUpdate()
+    {
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
         position.y = position.y + 0.1f * vertical;
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
-}
+    void }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
