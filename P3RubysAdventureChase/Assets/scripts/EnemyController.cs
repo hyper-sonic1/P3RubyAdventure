bc561bbc4f9bb42b8b523e277a32d7d52f3da6ec
hyper-sonic1 <145381195+hyper-sonic1@users.noreply.github.com>
2023-11-30 21:36:51 -0800

    Today i added animations and finished my maplayout
    i did alot today and im sarting with the cog bullet.

---
+++ P3RubysAdventureChase/Assets/scripts/EnemyController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2d;
+
+    float timer;
+    int direction = 1;
+
+    Animator animater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animater = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        if(vertical)
+        {
+            animater.SetFloat("Move X", 0);
+            animater.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            animater.SetFloat("Move X", direction);
+            animater.SetFloat("Move Y", 0);
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+
+
+        rigidbody2d.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
--- P3RubysAdventureChase/Assets/scripts/RubyController.cs
+++ P3RubysAdventureChase/Assets/scripts/RubyController.cs
@@ -19,42 +19,74 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
     }
-        // Update is called once per frame
-        void Update()
-        {
-            horizontal = Input.GetAxis("Horizontal");
-            vertical = Input.GetAxis("Vertical");
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-            if (isInvincible)
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
             {
-                invincibleTimer -= Time.deltaTime;
+                isInvincible = false;
             }
 
 
         }
-        void FixedUpdate()
-        {
-            Vector2 position = transform.position;
-            position.x = position.x + speed * horizontal;
-            position.y = position.y + speed * vertical;
-
-            rigidbody2d.MovePosition(position);
-        }
 
 
-
-
-        public void ChangeHealth(int amount)
-
-        {
-            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-            Debug.Log(currentHealth + "/" + maxHealth);
-        }
     }
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal;
+        position.y = position.y + speed * vertical;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+
+
+
+    public void ChangeHealth(int amount)
+
+    {
+        if (amount < 0)
+        {
+            animator.SetTrigger("Hit");
+
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
